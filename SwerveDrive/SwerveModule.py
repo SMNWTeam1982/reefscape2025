539fbde32aa69e40074941eebbf123adecccd4c2
Zach4346074545723 <3091322@smsd.org>
2024-11-07 17:08:25 +0000

    I just started implementing a pid controller for the turn motor, didnt change a ton of stuff

--- SwerveDrive/SwerveModule.py
+++ SwerveDrive/SwerveModule.py
@@ -2,6 +2,7 @@ import wpilib
 import rev._rev as rev
 from wpimath.kinematics import SwerveModuleState
 from wpimath.geometry import Rotation2d
+from wpimath.controller import PIDController
 
 from phoenix6 import hardware as ctre
 
@@ -29,8 +30,23 @@ class ModuleConstants:
     # error * kP
     DRIVE_MOTOR_PROPORTIONAL_GAIN = 0.0 # [untuned]
 
+    # also called kI
+    # accumulatedError * kI
+    DRIVE_MOTOR_INTEGRAL_GAIN = 0.0 # [untuned]
+
+    # also called kD
+    # rateError * kD
+    DRIVE_MOTOR_DERIVATIVE_GAIN = 0.0 # [untuned]
+
     # self explanitory (i hope)
     DRIVE_MOTOR_MAX_VELOCITY_METERS_PER_SECOND = 1.0 # [untuned]
+
+
+    # these coefficients must be positive
+    TURN_MOTOR_PROPORTIONAL_COEFFICIENT = 0.0 # [untuned]
+    TURN_MOTOR_INTEGRAL_COEFFICIENT = 0.0 # [untuned]
+    TURN_MOTOR_DERIVATIVE_COEFFICIENT = 0.0 # [untuned]
+    #TURN_MOTOR_FEEDFOREWARD = 0.0 # [untuned]
 
 
 
@@ -44,13 +60,19 @@ class SwerveModule:
         self.driveMotor = rev.CANSparkMax(driveMotorID, rev.CANSparkLowLevel.MotorType.kBrushless)
         self.turnMotor = rev.CANSparkMax(turnMotorID, rev.CANSparkLowLevel.MotorType.kBrushless)
         self.turnEncoder = ctre.CANcoder(turnEncoderID)
-        self.turnPID = () # TODO
+        self.turnPID = PIDController(
+            ModuleConstants.TURN_MOTOR_PROPORTIONAL_COEFFICIENT,
+            ModuleConstants.TURN_MOTOR_INTEGRAL_COEFFICIENT,
+            ModuleConstants.TURN_MOTOR_DERIVATIVE_COEFFICIENT
+        )
 
-    def getEncoder(self) -> Rotation2d:
+        
+
+    def getRotation(self) -> Rotation2d:
         return Rotation2d.fromRotations( self.turnEncoder.get_position() ) # relative position
         
     def run(self, unoptimezedDesiredState: SwerveModuleState):
-        currentAngle = self.getEncoder()
+        currentAngle = self.getRotation()
 
         OptimizedState = SwerveModuleState.optimize(unoptimezedDesiredState, currentAngle)
         OptimizedState.speed *= (OptimizedState.angle - currentAngle).cos()
