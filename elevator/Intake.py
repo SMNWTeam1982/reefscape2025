a82e9bebc60e287bb84a4a851f0ae9b9bded334b
Saltyrasin <128518844+Saltyrasin@users.noreply.github.com>
2025-01-25 11:14:43 -0600

    Update Intake.py
    Created the code for the Intake

--- elevator/Intake.py
+++ elevator/Intake.py
@@ -11,17 +11,97 @@ import rev
 from phoenix6 import hardware as ctre
 import wpimath.units
 
+
+        
+class IntakeConstants:
+    CORAL_INTAKE_MAX_SPEED = 0.0
+
+    LEVEL_1_WRIST_POSITION = 0.0
+    LEVEL_MID_WRIST_POSITION = 0.0
+    LEVEL_4_WRIST_POSITION = 0.0
+
+    ALGAE_INTAKE_MAX_SPEED = 0.0
+
+    ALGAE_OUT = 0.0
+
+    STOW = 0.0
+
+    WRIST_PROPORTIONAL_GAIN = 1.0
+    WRIST_INTEGRAL_GAIN = 0.0
+    WRIST_DERIVATIVE_GAIN = 0.0
+
+class WristTarget(enum.Enum):
+
+    L1 = IntakeConstants.LEVEL_1_WRIST_POSITION
+    LMID = IntakeConstants.LEVEL_MID_WRIST_POSITION
+    L4 = IntakeConstants.LEVEL_4_WRIST_POSITION
+
+    CoralMax = IntakeConstants.CORAL_INTAKE_MAX_SPEED
+    AlgaeMax = IntakeConstants.ALGAE_INTAKE_MAX_SPEED
+
+    AlgaeOut = IntakeConstants.ALGAE_OUT
+
+    Stow = IntakeConstants.STOW
+
+
+class IntakeState(enum.Enum):
+    Disabled = enum.auto
+    Moving = enum.auto
+
+
 class Intake:
     def __init__(self):
-        self.coralIntakeMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
-       
+        self.wristMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
+        self.wristEncoder = self.WristMotor.getEncoder()
 
-        self.WristMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
-        self.WristEncoder = self.WristEncoder.getEncoder()
+        self.rightAlgeaMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
 
-        self.AlgaeIntakeMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
-        
+        self.leftAlgaeMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
+
+        self.coralMotor = rev.CANSparkMax(0,rev.CANSparkLowLevel.MotorType.kBrushless)
 
 
+
+
+        self.wristPIDController = wpimath.controller.PIDController(
+            IntakeConstants.WRIST_PROPORTIONAL_GAIN,
+            IntakeConstants.WRIST_INTEGRAL_GAIN,
+            IntakeConstants.WRIST_DERIVATIVE_GAIN
+        )
+
+
+        self.intake = Intake()
+
+        self.state = IntakeState.Stopped
+        self.target = WristTarget.Intake
+
+
+    def getWristPosition(self) -> wpimath.units.meters:
+        wristPosition = (self.wristEncoder.getPosition())
+
+
+        return wristPosition 
+    
+    def moveWrist(self):
+
+        amount = self.wristPIDController.calculate(self.getWristPosition(),self.state.value)
+
+        self.wristMotor.run(amount)
+
+    def runWristController(self):
+        match self.state:
+            case IntakeState.Disabled:
+                self.wristMotor.stop()
+            case IntakeState.Moving:
+                self.moveElevator()
+            case _:
+                # this case should never be reached, we should put something up on telemetry
+                self.state = IntakeState.Stopped
+
+    def setState(self,state: IntakeState):
+        self.state = state
+
+    def wristTarget(self,target: WristTarget):
+        self.target = target
 
 
